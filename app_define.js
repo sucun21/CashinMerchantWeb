e05f8f0ac20396da5831654a1e66b1d49a8b4939
sucun21 <suk3c2uit@gmail.com>
2018-04-13 10:11:47 +0700

    change api url

--- app_define.js
+++ app_define.js
@@ -1,6 +1,10 @@
 //local
-var API_URL = 'http://localhost:8282/v001/mctransfer/web/api/';
+//var API_URL = 'http://localhost:8282/v001/mctransfer/web/api/';
 
 
 //test
-//vả API_URL = 'https://gbcdev.zing.vn/v001/mctransfer/web/api/';
+//var API_URL = 'https://gbcdev.zing.vn/v001/mctransfer/web/api/';
+
+
+//real
+var API_URL='https://zalopay.zing.vn/v001/mctransfer/web/api';
